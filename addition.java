e6d2ed8cb07574ecdcdc8b67cc73250fb45bc531
soniyadhupdale <159905417+soniyadhupdale@users.noreply.github.com>
2024-07-16 07:37:20 -0700

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,15 @@
+class addition
+{
+     public static void main(String[] args)
+     {
+          System.out.println("The sum of these numbers is : ");
+          
+          int num1 = 6;
+          int num2 = 5;
+          int num3 = 7;
+          int sum = num1 + num2 + num3;
+       
+          System.out.print(sum);
+     } 
+}
+
